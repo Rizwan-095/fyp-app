9ed3db33ac94c7de3467c717d8bcc76642812050
Muhammad Rizwan <awanrizwan615@gmail.com>
2025-06-18 20:46:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>Hi i am Rizwan</div>
-        <div>This is my fyp 2</div>
+        <div>This is my fyp 4</div>
       </header>
     </div>
   );
